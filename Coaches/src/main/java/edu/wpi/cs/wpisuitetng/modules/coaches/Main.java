9b38c9c71442830ca2d9646935a57a57b18aab32
Fredric Silberberg <fredric@fsilberberg.com>
2014-12-03 20:21:46 -0500

    Added jira and trello button

--- Coaches/src/main/java/edu/wpi/cs/wpisuitetng/modules/coaches/Main.java
+++ Coaches/src/main/java/edu/wpi/cs/wpisuitetng/modules/coaches/Main.java
@@ -11,24 +11,24 @@
 package edu.wpi.cs.wpisuitetng.modules.coaches;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 import javafx.application.Platform;
+import javafx.concurrent.Worker.State;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
 import javafx.scene.web.WebEngine;
-
 import javafx.scene.web.WebView;
 
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
 
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
@@ -37,61 +37,110 @@ import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
  * @author 333fr_000
  */
 public class Main implements IJanewayModule {
-    
-    /**
-     * A list of tabs owned by this module
-     */
-    List<JanewayTabModel> tabs;
-    
-    /**
-     * Variables, used for tests
-     */
-    JanewayTabModel tab1;
-    
-    private final JFXPanel mainPanel = new JFXPanel();
-    private WebEngine engine;
-    private WebView view;
-    
-    private final JPanel panel;
-    
-    /**
-     * Constructs the main views for this module. Namely one tab, containing
-     * a toolbar and a main content panel.
-     */
-    public Main() {
-        
-        // Initialize the list of tabs (however, this module has only one tab)
-        this.tabs = new ArrayList<JanewayTabModel>();
-        
-        panel = new JPanel(new BorderLayout());
-        panel.add(mainPanel, BorderLayout.CENTER);
-        Platform.runLater(() -> {
-        	view = new WebView();
-        	engine = view.getEngine();
-        	engine.setUserAgent("Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36");
-        	engine.load("http://trello.com");
-        	mainPanel.setScene(new Scene(view));
-        });
-        
-        tab1 = new JanewayTabModel("Coaches", null, new JLabel("Test 1"), panel);
-        
-        // Add the tab to the list of tabs owned by this module
-        this.tabs.add(tab1);
-    }
-    
-    /*
-     * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getName()
-     */
-    @Override
-    public String getName() {
-        return "Coaches";
-    }
-    
-    /*
-     * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getTabs()
-     */
-    @Override
-    public List<JanewayTabModel> getTabs() {
-        return tabs;
-    }
+
+	/**
+	 * A list of tabs owned by this module
+	 */
+	List<JanewayTabModel> tabs;
+
+	/**
+	 * Variables, used for tests
+	 */
+	JanewayTabModel tab1;
+
+	private final JFXPanel mainPanel = new JFXPanel();
+	private WebEngine engine;
+	private WebView view;
+
+	private final JPanel panel;
+	private final JPanel toolbar;
+	private final JProgressBar progressBar = new JProgressBar();
+	private final JButton trelloBtn = new JButton("Trello");
+	private final JButton jiraBtn = new JButton("Jira");
+
+	/**
+	 * Constructs the main views for this module. Namely one tab, containing a
+	 * toolbar and a main content panel.
+	 */
+	public Main() {
+
+		// Initialize the list of tabs (however, this module has only one tab)
+		this.tabs = new ArrayList<JanewayTabModel>();
+
+		panel = new JPanel(new BorderLayout());
+		panel.add(mainPanel, BorderLayout.CENTER);
+		Platform.runLater(() -> {
+			view = new WebView();
+			engine = view.getEngine();
+			engine.setUserAgent("Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36");
+			engine.load("http://trello.com");
+			mainPanel.setScene(new Scene(view));
+
+			// The progress bar
+			engine.getLoadWorker()
+					.workDoneProperty()
+					.addListener(
+							(observableValue, oldValue, newValue) -> {
+								SwingUtilities.invokeLater(() -> progressBar
+										.setValue(newValue.intValue()));
+							});
+
+			// If an error occurs
+			engine.getLoadWorker()
+					.exceptionProperty()
+					.addListener(
+							(o, old, value) -> {
+								if (engine.getLoadWorker().getState() == State.FAILED) {
+									SwingUtilities.invokeLater(() -> {
+										JOptionPane
+												.showMessageDialog(
+														panel,
+														(value != null) ? engine
+																.getLocation()
+																+ "\n"
+																+ value.getMessage()
+																: engine.getLocation()
+																		+ "\nUnexpected error.",
+														"Loading error...",
+														JOptionPane.ERROR_MESSAGE);
+									});
+								}
+								mainPanel.setScene(new Scene(view));
+							});
+		});
+
+		toolbar = new JPanel(new BorderLayout());
+		JPanel buttonPanel = new JPanel();
+		toolbar.add(buttonPanel, BorderLayout.CENTER);
+		toolbar.add(progressBar, BorderLayout.SOUTH);
+		buttonPanel.add(trelloBtn);
+		buttonPanel.add(jiraBtn);
+
+		trelloBtn.addActionListener((action) -> Platform.runLater(() -> engine
+				.load("http://trello.com")));
+
+		jiraBtn.addActionListener((action) -> Platform.runLater(() -> engine
+				.load("https://jira.atlassian.com/secure/Dashboard.jspa")));
+
+		tab1 = new JanewayTabModel("Coaches", null, toolbar, panel);
+
+		// Add the tab to the list of tabs owned by this module
+		this.tabs.add(tab1);
+	}
+
+	/*
+	 * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getName()
+	 */
+	@Override
+	public String getName() {
+		return "Coaches";
+	}
+
+	/*
+	 * @see edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule#getTabs()
+	 */
+	@Override
+	public List<JanewayTabModel> getTabs() {
+		return tabs;
+	}
 }
